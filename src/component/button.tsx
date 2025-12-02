c0512d6ae92ad94be63ea0992b0d0604a03aaceb
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-02 13:25:41 +0530

    feat: infer and props

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+export default function Button (props : {backgroundColor : string}) {
 
-export default function Button () {
+    const bgColor = props.backgroundColor;
+    // or
+    const {backgroundColor} = props
+
     return (
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Button
