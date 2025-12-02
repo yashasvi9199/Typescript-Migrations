8da8c3acdfed6b5c145710b0d316bc3e7017970d
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-02 14:38:37 +0530

    feat: passing function as type

---
+++ src/component/button10.tsx
@@ -0,0 +1,14 @@
+type ButtonProps = {
+    onClick: () => void,
+    onSubmit: (test: string) => number
+}
+
+
+export default function Button03 ({onClick, onSubmit} : ButtonProps) {
+    return (
+        <button onClick={onClick}       //we can have onSubmit here as well but onClick doesn't support that type of returns here so not adding in
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+            Button10
+        </button>
+    )
+}
