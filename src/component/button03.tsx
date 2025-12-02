90966bd99807b5cd159097566b1d01fed5e609a0
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-02 13:26:37 +0530

    feat: union and types

---
+++ src/component/button03.tsx
@@ -0,0 +1,13 @@
+type ButtonProps = {
+    backgroundColor : "red" | "green" | "blue",     // union
+    fontSize: number,
+    pillShape: boolean
+}
+
+export default function Button03 ({backgroundColor, fontSize, pillShape} : ButtonProps) {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    )
+}
