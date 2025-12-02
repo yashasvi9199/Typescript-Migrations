4e13994af042e9dea590f9a25a2316452e2443ce
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-02 15:17:05 +0530

    feat: type Children

---
+++ src/component/div01.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type Props = {
+    children? : React.ReactNode,    // accepts text, JSX elements and everything
+    child?: React.ReactElement      // accepts only JSX elements
+}
+export default function Div({children} : Props) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
