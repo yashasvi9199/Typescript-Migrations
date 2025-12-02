37d8d081dca5060c54d4a482dc9a0d8364c8f781
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-02 13:31:08 +0530

    feat: array type

---
+++ src/component/button05.tsx
@@ -0,0 +1,12 @@
+type ButtonProps = {
+    padding: number[]
+}
+
+
+export default function Button03 ({padding} : ButtonProps) {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    )
+}
